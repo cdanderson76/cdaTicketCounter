6bf312dd8616c13c3459482294661c2b4bdbb022
Clifford Anderson <138369806+cdanderson76@users.noreply.github.com>
2026-03-30 11:05:49 -0500

    Add decrement and reset functions to counter

--- index.js
+++ index.js
@@ -7,9 +7,22 @@ function increment() {
     countEl.textContent = count
 }
 
+function decrement() {
+    if( count > 0 ) {
+        count -= 1
+        countEl.textContent = count
+    }
+}
+
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+}
+
+function reset() {
+    count = 0;
+    countEl.textContent = count
+    saveEl.textContent = ''
 }
